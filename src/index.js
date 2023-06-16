917d35a966782569ee90ebde3f8c922081c1ab03
Yonashenok <yonashenok24@gmail.com>
2023-06-16 06:08:12 +0300

    feat: imported remove all feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style/index.css';
 import renderListItem from './renderListItem.js';
 import addNewListItem from './addNewListItem.js';
+import allRemoveHandler from './removeAllCompleted.js';
 
 const init = () => {
   renderListItem();
 };
 init();
 const form = document.querySelector('.to-do-form');
+const removeBtn = document.querySelector('.to-do--btn');
+
 form.addEventListener('submit', addNewListItem);
+removeBtn.addEventListener('click', allRemoveHandler);
