3df666a9126537bb91f8d01fe14ee5019cbdef85
Yonashenok <yonashenok24@gmail.com>
2023-06-16 05:12:50 +0300

    feat: remove List Item with index featuer

--- src/removeListItem.js
+++ src/removeListItem.js
@@ -2,11 +2,10 @@ import getLocalStorage from './getLocalStorage.js';
 import setLocalStorage from './setLocalStorage.js';
 import renderListItem from './renderListItem.js';
 
-const removeListItem = (e) => {
-  const removeIndex = e.target.parentElement.dataset.toggle;
+const removeListItem = (removeIndex) => {
   const toData = getLocalStorage();
-
-  const newData = toData.filter((item) => item.index !== +removeIndex);
+  console.log(removeIndex);
+  const newData = toData.filter((item) => item.index !== removeIndex);
   for (let i = 0; i < newData.length; i += 1) {
     newData[i].index = i;
   }
