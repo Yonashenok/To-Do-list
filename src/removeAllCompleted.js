cf134ed3ef95b79b75d588412dc046f167303a50
Yonashenok <yonashenok24@gmail.com>
2023-06-16 06:34:50 +0300

    feat: added to update index

--- src/removeAllCompleted.js
+++ src/removeAllCompleted.js
@@ -5,6 +5,9 @@ import setLocalStorage from './setLocalStorage.js';
 const allRemoveHandler = () => {
   const toData = getLocalStorage();
   const newData = toData.filter((item) => item.completed !== true);
+  for (let i = 0; i < newData.length; i += 1) {
+    newData[i].index = i;
+  }
   setLocalStorage(newData);
   renderListItem();
 };
