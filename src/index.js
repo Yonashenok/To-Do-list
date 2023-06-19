77de14430fb0620666113941cfbbc2008b0125d6
Yonashenok <yonashenok24@gmail.com>
2023-06-19 12:02:56 +0300

    feat: modal handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style/index.css';
+import './style/index.scss';
 import renderListItem from './renderListItem.js';
 import addNewListItem from './addNewListItem.js';
 import removeHandler from './removeHandler.js';
@@ -8,9 +8,21 @@ const init = () => {
 };
 init();
 const form = document.querySelector('.to-do-form');
+const overly = document.querySelector('.overly');
 const removeBtn = document.querySelector('.to-do--btn');
+const createBtn = document.querySelector('.create-to-btn');
+const btnClose = document.querySelector('.btn-close');
+createBtn.addEventListener('click', () => {
+  overly.classList.toggle('hidden');
+});
+form.addEventListener('submit', (e) => {
+  addNewListItem(e);
+  overly.classList.toggle('hidden');
+});
 
-form.addEventListener('submit', addNewListItem);
+btnClose.addEventListener('click', () => {
+  overly.classList.toggle('hidden');
+});
 removeBtn.addEventListener('click', () => {
   removeHandler(null);
   renderListItem();
