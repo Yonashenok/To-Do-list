23de32b83f365e332d184f91db4c13ed7dd5eee0
Yonashenok <yonashenok24@gmail.com>
2023-06-23 14:36:59 +0300

    feat: event listener for navgetion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style/index.scss';
+import getLocalStorage from './getLocalStorage.js';
 import renderListItem from './renderListItem.js';
 import addNewListItem from './addNewListItem.js';
 import removeHandler from './removeHandler.js';
+import markUncompleted from './markUncompleted.js';
+
+const allCont = document.querySelector('.to-do-list--container');
 
 const init = () => {
-  renderListItem();
+  const toData = getLocalStorage();
+
+  renderListItem(toData, allCont);
+  markUncompleted();
 };
 init();
 const form = document.querySelector('.to-do-form');
@@ -12,6 +19,9 @@ const overly = document.querySelector('.overly');
 const removeBtn = document.querySelector('.to-do--btn');
 const createBtn = document.querySelector('.create-to-btn');
 const btnClose = document.querySelector('.btn-close');
+const navLink = document.querySelectorAll('.to-do-nav-link');
+const navContainer = document.querySelector('.to-do-nav');
+const timeDate = document.querySelector('.time-date');
 createBtn.addEventListener('click', () => {
   overly.classList.toggle('hidden');
 });
@@ -25,5 +35,32 @@ btnClose.addEventListener('click', () => {
 });
 removeBtn.addEventListener('click', () => {
   removeHandler(null);
-  renderListItem();
+  const toData = getLocalStorage();
+  renderListItem(toData, allCont);
+  markUncompleted();
 });
+
+navContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.to-do-nav-link');
+  if (!clicked) return;
+  navLink.forEach((link) => {
+    link.classList.remove('OnClickLink');
+  });
+
+  clicked.classList.add('OnClickLink');
+});
+
+const setTime = () => {
+  const date = new Date();
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+  timeDate.textContent = new Intl.DateTimeFormat('en-GB', options).format(date);
+};
+
+setInterval(setTime, 100);
